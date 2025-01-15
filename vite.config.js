641fc9b1d405a45f572f4431922748ca73042287
Divya Prakash Jha <prakashdivya0607@gmail.com>
2025-01-15 16:25:05 +0530

    config mods

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         // target: 'http://localhost:3000',
         target: 'https://skillmingle-backend.onrender.com',
         changeOrigin: true,
-        secure: false,
+        secure: import.meta.env.VITE_ENV === "production" ? true : false,
       },
     },
   },
